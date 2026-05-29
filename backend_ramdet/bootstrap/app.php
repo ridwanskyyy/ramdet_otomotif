5f32bc2f6df83566f090ba3ccbd0fe7b69b58b75
Ridwan Nur Heriyanto <nurridwan978@gmail.com>
2026-05-29 21:03:54 +0700

    feat: update app configuration and middleware settings

--- backend_ramdet/bootstrap/app.php
+++ backend_ramdet/bootstrap/app.php
@@ -3,6 +3,8 @@
 use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
+use Illuminate\Auth\AuthenticationException; 
+use Illuminate\Http\Request; 
 
 return Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
@@ -15,5 +17,15 @@ return Application::configure(basePath: dirname(__DIR__))
         //
     })
     ->withExceptions(function (Exceptions $exceptions): void {
-        //
+        
+        $exceptions->render(function (AuthenticationException $e, Request $request) {
+            if ($request->is('api/*')) {
+                return response()->json([
+                    'success' => false,
+                    'message' => 'Gagal memproses! Anda harus login terlebih dahulu.',
+                    'data' => null
+                ], 401);
+            }
+        });
+
     })->create();
