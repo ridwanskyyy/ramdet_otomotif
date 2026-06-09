3700cc0ea1c7875d592f32ae1e5447f39887551f
Ramsnalog <r4m4naufal@gmail.com>
2026-06-09 23:25:17 +0700

    Feat: Reorganisasi struktur folder frontend dan update rute API

--- frontend_ramdet/macos/Flutter/GeneratedPluginRegistrant.swift
+++ frontend_ramdet/macos/Flutter/GeneratedPluginRegistrant.swift
@@ -5,16 +5,10 @@
 import FlutterMacOS
 import Foundation
 
-<<<<<<< Updated upstream
 import file_selector_macos
 import flutter_secure_storage_macos
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
   FileSelectorPlugin.register(with: registry.registrar(forPlugin: "FileSelectorPlugin"))
-=======
-import flutter_secure_storage_macos
-
-func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
->>>>>>> Stashed changes
   FlutterSecureStoragePlugin.register(with: registry.registrar(forPlugin: "FlutterSecureStoragePlugin"))
 }
